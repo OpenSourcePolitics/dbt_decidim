c719a822714e8a5b1f5c7bdbd1601daf22d5c87e
data@opensourcepolitics.eu <data@opensourcepolitics.eu>
2022-09-09 14:49:06 +0000

    Add organization model

---
+++ models/organization.sql
@@ -0,0 +1,18 @@
+select id,
+    name,
+    host,
+    default_locale,
+    available_locales,
+    created_at,
+    updated_at,
+    description->>'fr' as description,
+    secondary_hosts,
+    available_authorizations,
+    header_snippets,
+    tos_version,
+    badges_enabled,
+    send_welcome_notification,
+    users_registration_mode,
+    time_zone
+from decidim_organizations
+where host = '{{ env_var('DBT_DECIDIM_HOST') }}'
