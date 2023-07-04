9cd99cd43173e0919e4d7417b3e13d10b225a430
THIVEND <baptiste.thivend@protonmail.com>
2023-07-04 12:07:24 +0200

    Add debates staging

---
+++ models/staging/decidim/stg_debates.sql
@@ -0,0 +1,16 @@
+select
+    id,
+    title->>'{{ env_var('DBT_LANGUAGE_CODE') }}' as title,
+    regexp_replace(
+        description->>'{{ env_var('DBT_LANGUAGE_CODE') }}',
+        E'(<[^>]+>)|(&[a-z]+;)', '', 'gi'
+    ) as description,
+    instructions->>'{{ env_var('DBT_LANGUAGE_CODE') }}' as instructions,
+    start_time,
+    end_time,
+    closed_at,
+    decidim_component_id,
+    decidim_author_id,
+    created_at,
+    'Decidim::Debates::Debate' as resource_type
+from decidim_debates_debates
