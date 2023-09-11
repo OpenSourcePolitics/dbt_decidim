cb0ff168155318e74b4968db016139697be9d200
THIVEND <baptiste.thivend@protonmail.com>
2023-09-11 16:58:17 +0200

    Add stg_proposals_collaborative_drafts

---
+++ models/staging/decidim/stg_proposals_collaborative_drafts.sql
@@ -0,0 +1,21 @@
+select
+    id,
+    title,
+    body,
+    decidim_component_id,
+    decidim_scope_id,
+    state,
+    reference,
+    address,
+    latitude,
+    longitude,
+    published_at,
+    authors_count,
+    versions_count,
+    contributions_count,
+    created_at,
+    updated_at,
+    coauthorships_count,
+    comments_count,
+    follows_count
+from {{ source('decidim', 'decidim_proposals_collaborative_drafts') }}
